7aa0d3ebaf375f1d1f68e82cb26d767fabb69ca2
yun1989yue <418292922@qq.com>
2015-11-25 20:43:16 -0800

    Update path sum.py

--- path sum.py
+++ path sum.py
@@ -39,3 +39,67 @@ class Solution(object):
                     nextNodes.append(node.right)
             stack = nextNodes
         return False
+'''
+Method: recursion O(n) time O(n) or O(logn) space?
+'''
+class Solution(object):
+    def hasPathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: bool
+        """
+        if not root:
+            return False
+        if not root.left and not root.right:
+            return root.val == sum
+        return self.hasPathSum(root.left, sum-root.val) or self.hasPathSum(root.right, sum-root.val)
+        
+'''
+Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
+
+For example:
+Given the below binary tree and sum = 22,
+              5
+             / \
+            4   8
+           /   / \
+          11  13  4
+         /  \    / \
+        7    2  5   1
+return
+[
+   [5,4,11,2],
+   [5,8,4,5]
+]
+'''
+'''
+Method: recursion O(n) time
+'''
+class Solution(object):
+    def pathSum(self, root, sum):
+        """
+        :type root: TreeNode
+        :type sum: int
+        :rtype: List[List[int]]
+        """
+        self.res = []
+        self.helper(root, [], sum)
+        return self.res
+    
+    def helper(self, root, current, sum): # current is a list of value of upper nodes on the path, sum is remained sum exclude values of upper nodes
+        if not root:
+            return 
+        if not root.left and not root.right:
+            if root.val == sum:
+                key = current[:]
+                key.append(root.val)
+                self.res.append(key)
+        if root.left:
+            key = current[:]
+            key.append(root.val)
+            self.helper(root.left, key, sum - root.val)
+        if root.right:
+            key = current[:]
+            key.append(root.val)
+            self.helper(root.right, key, sum - root.val)
