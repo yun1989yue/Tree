2202d3844c6fd2d0c5974a1c147ffb4910cd7418
yun1989yue <418292922@qq.com>
2015-12-14 01:38:02 -0800

    Update validate binary search tree.py

--- validate binary search tree.py
+++ validate binary search tree.py
@@ -32,3 +32,46 @@ class Solution(object):
 M2:
 Get inorder traversal in stack, and judge whether it is increasing 
 '''
+'''
+M3: inorder recursion O(n) time O(logn) space
+'''
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        self.pre = None # reason for self.pre is if pre is parameter, it comes from upper level, but it should come from lower level
+        self.res = True
+        self.explore(root) 
+        return self.res
+        
+    def explore(self, root):
+        if not root:
+            return
+        self.explore(root.left)
+        if self.pre and self.pre.val >= root.val:
+                self.res = False
+        self.pre = root
+        self.explore(root.right)
+        
+class Solution(object):
+    def isValidBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: bool
+        """
+        self.pre = None
+        return self.explore(root)
+        
+    def explore(self, root):
+        if not root:
+            return True
+        if not self.explore(root.left):
+            return False
+        if self.pre and self.pre.val >= root.val:
+                return False
+        self.pre = root
+        if not self.explore(root.right):
+            return False
+        return True
